7dd3b597323f0532b24940aa947a7b4ed3b604b6
cameronmckenzie <cameronctc11@gmail.com>
2025-09-26 13:22:06 -0600

    Initial Commit

---
+++ ColoradoWeather/ColoradoWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColoradoWeatherApp.swift
+//  ColoradoWeather
+//
+//  Created by McKenzie, Cameron - Student on 9/26/25.
+//
+
+import SwiftUI
+
+@main
+struct ColoradoWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColoradoWeatherTests/ColoradoWeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  ColoradoWeatherTests.swift
+//  ColoradoWeatherTests
+//
+//  Created by McKenzie, Cameron - Student on 9/26/25.
+//
+
+import Testing
+@testable import ColoradoWeather
+
+struct ColoradoWeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
